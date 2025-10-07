79df05a8975fba9c37c648e6fcfd4a1cd3dfb3d0
SkimerPM <jose.sanchez.q@tecsup.edu.pe>
2025-10-07 12:32:30 -0500

    fix: Corregido STATIC_URL a '/static/' para resolver el error 404 de archivos estáticos

--- gestionrestaurante/settings.py
+++ gestionrestaurante/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
